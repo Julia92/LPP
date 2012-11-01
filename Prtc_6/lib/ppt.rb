41a9fdad25f2f1a644a37244f39ae8267e4aaa6c
Julia92 <lagunerita@hotmail.com>
2012-11-01 12:28:06 +0000

    Sexto cambio Practica 6

--- Prtc_6/lib/ppt.rb
+++ Prtc_6/lib/ppt.rb
@@ -4,11 +4,18 @@ class PiedraPapelTijera
    attr_accessor :humano, :maquina, :tiradas, :ganadoras
 
     def initialize  
-      @humano = 0
-      @maquina = 0
+      @humano = :tijeras
+      @maquina = :papel
       @tiradas = [ :piedra, :papel, :tijeras]
       @ganadoras={:piedra=>:tijeras , :papel=>:piedra, :tijeras=>:papel}
 
     end
+
+   def obtener_humano(jugada)
+     @humano=jugada.to_sym
+   end
+   def obtener_maquina
+     @maquina=@tiradas.sample
+   end
 end
 
--- Prtc_6/spec/ppt_spec.rb
+++ Prtc_6/spec/ppt_spec.rb
@@ -8,11 +8,11 @@ require 'ppt'
      end
     
           it "Debe existir una tirada para el humano" do
-             @ppt_obj.humano.should == 0
+             @ppt_obj.humano.should == :tijeras
           end
           
 	  it "Debe existir una tirada para la maquina" do
-	     @ppt_obj.maquina.should == 0
+	     @ppt_obj.maquina.should == :papel
           end
 
 	  it "Debe existir una lista de tiradas validas" do
@@ -22,6 +22,20 @@ require 'ppt'
           it "Debe existir una lista de jugadas posibles y quien gana" do
              @ppt_obj.ganadoras.should=={:piedra=>:tijeras , :papel=>:piedra, :tijeras=>:papel}
           end
+
+          it "Se debe invocar al metodo obtener_humano() para recoger la tirada del humano y que esta sea valida" do
+             @ppt_obj.obtener_humano('piedra').should == @ppt_obj.humano
+          end
+
+          it "Se debe invocar al metodo obtener_maquina() para recoger la tirada de la maquina y que esta sea valida" do
+             @ppt_obj.obtener_maquina.should==@ppt_obj.maquina
+          end
+
+
+
+
+
+
 
 end
     
